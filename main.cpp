9417cc1006b5fde49907b434a8265ea81cc6a74f
adamabid1 <99610892+adamabid1@users.noreply.github.com>
2023-11-29 13:43:45 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    string Name;
+    string location;
+    int age = 0;
+    
+    
+    cout << "Please enter your Information below" << endl;
+    cout << "Name " ;
+    cin >> Name ;
+    cout << "Welcome  " << Name << endl;
+    
+    
+    
+    
+}
